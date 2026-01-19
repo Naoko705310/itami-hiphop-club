32cdb0e974e5109d6e34e13cfdd4cc2c6da622c4
Naoko705310 <naokos79@gmail.com>
2026-01-19 22:46:51 +0900

    レッスン紹介カードの内容を修正

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,24 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
 
+  // お問い合わせフォームのラジオボタン切り替え
+  const $radioButtons = $('input[name="inquiry-type"]');
+  const $trialFields = $('.contact__form-item--trial');
+
+  // 初期表示の設定
+  function updateFormFields() {
+    const selectedValue = $('input[name="inquiry-type"]:checked').val();
+    if (selectedValue === 'trial') {
+      $trialFields.show();
+    } else {
+      $trialFields.hide();
+    }
+  }
+
+  // ページ読み込み時に実行
+  updateFormFields();
+
+  // ラジオボタン変更時に実行
+  $radioButtons.on('change', updateFormFields);
 
 });
