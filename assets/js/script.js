845455dd5ea35cc13850901684401f2f1fba1d1f
Naoko705310 <naokos79@gmail.com>
2026-01-19 22:54:14 +0900

    FVセクションのテキストとスタイルを調整
    - テキスト内容を「50代~70代の方を中心にした」に変更
    - FVのテキストフォントサイズをモバイルでも18pxに統一

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,25 @@
 "use strict";
 
-jQuery(function ($) {// この中であればWordpressでも「$」が使用可能になる
+jQuery(function ($) {
+  // この中であればWordpressでも「$」が使用可能になる
+
+  // お問い合わせフォームのラジオボタン切り替え
+  var $radioButtons = $('input[name="inquiry-type"]');
+  var $trialFields = $('.contact__form-item--trial');
+
+  // 初期表示の設定
+  function updateFormFields() {
+    var selectedValue = $('input[name="inquiry-type"]:checked').val();
+    if (selectedValue === 'trial') {
+      $trialFields.show();
+    } else {
+      $trialFields.hide();
+    }
+  }
+
+  // ページ読み込み時に実行
+  updateFormFields();
+
+  // ラジオボタン変更時に実行
+  $radioButtons.on('change', updateFormFields);
 });
